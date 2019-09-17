98d26bbf90bfa9cc016180f924cbd0020ff2a441
Miguel E. Ruiz <meruiz77@gmail.com>
2019-09-17 19:57:26 +0200

    Refactor Berries module

--- lib/Pokedex/berries.ex
+++ lib/Pokedex/berries.ex
@@ -1,11 +1,8 @@
 defmodule Pokedex.Berries do
-    def berry(id) when is_number(id) do
-        Pokedex.Url.get("/berry/" <> to_string(id))
+    def berry(id) when is_number(id) or is_atom(id) do
+        Pokedex.get("/berry/" <> to_string(id))
     end
     def berry(name) when is_bitstring(name) do
-        Pokedex.Url.get("/berry/" <> name)
-    end
-    def berry(name) when is_atom(name) do
-        Pokedex.Url.get("/berry/" <> to_string(name))
+        Pokedex.get("/berry/" <> name)
     end
 end
