4571a2fa3e5897321564b6690e2f0f39a9a8b7a1
Miguel E. Ruiz <meruiz77@gmail.com>
2019-09-17 21:57:52 +0200

    Add berries flavor functionality

--- lib/Pokedex/berries.ex
+++ lib/Pokedex/berries.ex
@@ -16,4 +16,11 @@ defmodule Pokedex.Berries do
     def berry_firmness(name) when is_bitstring(name) do
         Pokedex.get("/berry-firmness/" <> name)
     end
+
+    def berry_flavor(id) when is_number(id) or is_atom(id) do
+        Pokedex.get("/berry-flavor/" <> to_string(id))
+    end
+    def berry_flavor(name) when is_bitstring(name) do
+        Pokedex.get("/berry-flavor/" <> name)
+    end
 end
