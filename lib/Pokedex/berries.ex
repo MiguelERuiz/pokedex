c9bdf6d1c7049da70941bb6505ec60014d29f30c
Miguel E. Ruiz <meruiz77@gmail.com>
2019-09-17 20:56:22 +0200

    Add berries firmness functionality

--- lib/Pokedex/berries.ex
+++ lib/Pokedex/berries.ex
@@ -5,4 +5,15 @@ defmodule Pokedex.Berries do
     def berry(name) when is_bitstring(name) do
         Pokedex.get("/berry/" <> name)
     end
+
+    def berry_firmness(id) when is_number(id) do
+        Pokedex.get("/berry-firmness/" <> to_string(id))
+    end
+    def berry_firmness(name) when is_atom(name) do
+        name = (name |> Atom.to_string) |> String.replace("_", "-")
+        Pokedex.get("/berry-firmness/" <> name)
+    end
+    def berry_firmness(name) when is_bitstring(name) do
+        Pokedex.get("/berry-firmness/" <> name)
+    end
 end
