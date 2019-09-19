37b16fe239562a026087f6d3cc61b6cd1848c8dc
Miguel E. Ruiz <meruiz77@gmail.com>
2019-09-19 23:56:41 +0200

    Add Contests Module
    All functionalities with ExUnit tests

---
+++ test/contests_test.exs
@@ -0,0 +1,76 @@
+defmodule ContestsTest do
+  use ExUnit.Case
+
+  test "get contest type by numeric id" do
+    assert {:ok, env} = Pokedex.Contests.contest_type(1)
+    assert env.status == 200
+    assert env.body["name"] == "cool"
+  end
+
+  test "get contest type by string id" do
+    assert {:ok, env} = Pokedex.Contests.contest_type("1")
+    assert env.status == 200
+    assert env.body["name"] == "cool"
+  end
+
+  test "get contest type by atom name" do
+    assert {:ok, env} = Pokedex.Contests.contest_type(:cool)
+    assert env.status == 200
+    assert env.body["name"] == "cool"
+  end
+
+  test "get contest type by string name" do
+    assert {:ok, env} = Pokedex.Contests.contest_type("cool")
+    assert env.status == 200
+    assert env.body["name"] == "cool"
+  end
+
+  test "get contest effect by numeric id" do
+    assert {:ok, env} = Pokedex.Contests.contest_effect(1)
+    assert env.status == 200
+    assert env.body["id"] == 1
+  end
+
+  test "get contest effect by string id" do
+    assert {:ok, env} = Pokedex.Contests.contest_effect("1")
+    assert env.status == 200
+    assert env.body["id"] == 1
+  end
+
+  test "get super contest effect by numeric id" do
+    assert {:ok, env} = Pokedex.Contests.super_contest_effect(1)
+    assert env.status == 200
+    assert env.body["id"] == 1
+  end
+
+  test "get super contest effect by string id" do
+    assert {:ok, env} = Pokedex.Contests.super_contest_effect("1")
+    assert env.status == 200
+    assert env.body["id"] == 1
+  end
+
+  test "get not found response for a not found numeric contest type" do
+    assert {:ok, env} = Pokedex.Contests.contest_type(3000)
+    assert env.status == 404
+  end
+
+  test "get not found response for a not found string contest type" do
+    assert {:ok, env} = Pokedex.Contests.contest_type("not cool")
+    assert env.status == 404
+  end
+
+  test "get not found response for a not found atomic contest type" do
+    assert {:ok, env} = Pokedex.Contests.contest_type(:not_cool)
+    assert env.status == 404
+  end
+
+  test "get not found response for a not found contest effect" do
+    assert {:ok, env} = Pokedex.Contests.contest_effect(3000)
+    assert env.status == 404
+  end
+
+  test "get not found response for a not found super contest effect" do
+    assert {:ok, env} = Pokedex.Contests.super_contest_effect(3000)
+    assert env.status == 404
+  end
+end
