b3e3ea0b952ab722261a952dbd752bb6cd95bcc6
Miguel E. Ruiz <meruiz77@gmail.com>
2019-09-21 16:09:10 +0200

    Add Evolution Module
    All functionalities with ExUnit tests

---
+++ lib/Pokedex/evolution.ex
@@ -0,0 +1,19 @@
+defmodule Pokedex.Evolution do
+  def evolution_chain(id) when is_number(id) do
+    Pokedex.get("/evolution-chain/" <> to_string(id))
+  end
+  def evolution_chain(name) when is_bitstring(name) do
+    Pokedex.get("/evolution-chain/" <> name)
+  end
+
+  def evolution_trigger(id) when is_number(id) do
+    Pokedex.get("/evolution-trigger/" <> to_string(id))
+  end
+  def evolution_trigger(name) when is_atom(name) do
+    name = Pokedex.atom_to_pokedex_string(name)
+    Pokedex.get("/evolution-trigger/" <> name)
+  end
+  def evolution_trigger(name) when is_bitstring(name) do
+    Pokedex.get("/evolution-trigger/" <> name)
+  end
+end
---
+++ test/evolution_test.exs
@@ -0,0 +1,43 @@
+defmodule EvolutionTest do
+  use ExUnit.Case, async: true
+
+  test "get evolution chain by numeric id" do
+    assert {:ok, env} = Pokedex.Evolution.evolution_chain(1)
+    assert env.status == 200
+    assert env.body["id"] == 1
+  end
+
+  test "get evolution chain by string id" do
+    assert {:ok, env} = Pokedex.Evolution.evolution_chain("1")
+    assert env.status == 200
+    assert env.body["id"] == 1
+  end
+
+  test "get evolution trigger by numeric id" do
+    assert {:ok, env} = Pokedex.Evolution.evolution_trigger(1)
+    assert env.status == 200
+    assert env.body["name"] == "level-up"
+  end
+
+  test "get evolution trigger by atom name" do
+    assert {:ok, env} = Pokedex.Evolution.evolution_trigger(:level_up)
+    assert env.status == 200
+    assert env.body["name"] == "level-up"
+  end
+
+  test "get evolution trigger by string name" do
+    assert {:ok, env} = Pokedex.Evolution.evolution_trigger("level-up")
+    assert env.status == 200
+    assert env.body["name"] == "level-up"
+  end
+
+  test "get not found response for a not found evolution chain" do
+    assert {:ok, env} = Pokedex.Evolution.evolution_chain(-1)
+    assert env.status == 404
+  end
+
+  test "get not found response for a not found evolution trigger" do
+    assert {:ok, env} = Pokedex.Evolution.evolution_trigger(:not_an_evolution_trigger)
+    assert env.status == 404
+  end
+end
