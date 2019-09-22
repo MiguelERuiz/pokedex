90c33974bcb05a3b29b279096e86aa4090375126
Miguel E. Ruiz <meruiz77@gmail.com>
2019-09-23 00:12:45 +0200

    Bump version 1.0.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Pokedex.Mixfile do
   def project do
     [
       app: :pokedex,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
